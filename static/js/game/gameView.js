f60c0b9e0746331c6251919314f5bc9be1e84312
angerscroll <angerscroll@gmail.com>
2022-11-03 23:00:53 +0900

    add css

--- static/js/game/gameView.js
+++ static/js/game/gameView.js
@@ -12,6 +12,33 @@ export default class GameView {
           </span>
         </button>
       </div>
+      <div class="board">
+        <div class="board__tile" data-index="0">X</div>
+        <div class="board__tile" data-index="1">X</div>
+        <div class="board__tile board__tile--check" data-index="2"></div>
+        <div class="board__tile" data-index="3">X</div>
+        <div class="board__tile" data-index="4">X</div>
+        <div class="board__tile" data-index="5">X</div>
+        <div class="board__tile board__tile--check" data-index="6"></div>
+        <div class="board__tile board__tile--check" data-index="7"></div>
+        <div class="board__tile" data-index="8">X</div>
+        <div class="board__tile" data-index="9">X</div>
+        <div class="board__tile" data-index="10">X</div>
+        <div class="board__tile" data-index="11">X</div>
+        <div class="board__tile board__tile--check" data-index="12"></div>
+        <div class="board__tile" data-index="13">X</div>
+        <div class="board__tile" data-index="14">X</div>
+        <div class="board__tile" data-index="15">X</div>
+        <div class="board__tile" data-index="16">X</div>
+        <div class="board__tile board__tile--check" data-index="17"></div>
+        <div class="board__tile" data-index="18">X</div>
+        <div class="board__tile" data-index="19">X</div>
+        <div class="board__tile" data-index="20">X</div>
+        <div class="board__tile board__tile--check" data-index="21"></div>
+        <div class="board__tile board__tile--check" data-index="22"></div>
+        <div class="board__tile board__tile--check" data-index="23"></div>
+        <div class="board__tile" data-index="24">X</div>
+      </div>
     `
   }
 }
